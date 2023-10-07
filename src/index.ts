17fc381d2ee0886791ec51e99b8fbf1970d830d1
rocawear <anttivep@gmail.com>
2023-10-07 23:23:49 +0300

    update logic

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
-import {
-  Extension,
-  HDirection,
-  HMessage,
-  HEntity,
-  HPacket,
-  GAsync,
-  AwaitingPacket,
-} from "gnode-api";
+import { Extension, HDirection, HEntity, HPacket, HMessage } from "gnode-api";
 
 const ext = new Extension({
   author: "",
@@ -15,32 +7,44 @@ const ext = new Extension({
   version: "",
 });
 
-let userId: number;
-
-const gAsync = new GAsync(ext);
 ext.run();
 
-ext.interceptByNameOrHash(HDirection.TOCLIENT, "Users", async (hMessage) => {
-  const awaitedPacket = await gAsync.awaitPacket(
-    new AwaitingPacket("GetGuestRoomResult", HDirection.TOCLIENT, 1000)
-  );
+let isRoomLoaded = false;
+const users = new Set<number>();
 
-  if (!awaitedPacket) return;
-
+const onUsers = (hMessage: HMessage) => {
+  if (!isRoomLoaded) setTimeout(() => onUsers(hMessage), 100);
   const entities = HEntity.parse(hMessage.getPacket());
 
-  for (const entity of entities) {
-    if (entity.id === userId) return;
-    const packet = new HPacket("AvatarEffect", HDirection.TOCLIENT)
-      .appendInt(entity.index)
-      .appendInt(13)
-      .appendInt(0);
-    ext.sendToClient(packet);
-  }
-});
+  entities.forEach((entity) => {
+    if (!users.has(entity.index) && entity.index !== userId) {
+      users.add(entity.index);
+      const packet = new HPacket("AvatarEffect", HDirection.TOCLIENT)
+        .appendInt(0)
+        .appendInt(13)
+        .appendInt(0);
+      ext.sendToClient(packet);
+    }
+  });
+};
 
+ext.interceptByNameOrHash(HDirection.TOCLIENT, "Users", onUsers);
+
+let userId: number;
 ext.interceptByNameOrHash(
   HDirection.TOCLIENT,
   "UserObject",
   (hMessage) => (userId = hMessage.getPacket().readInteger())
+);
+
+ext.interceptByNameOrHash(
+  HDirection.TOCLIENT,
+  "GetGuestRoomResult",
+  () => (isRoomLoaded = true)
+);
+
+ext.interceptByNameOrHash(
+  HDirection.TOCLIENT,
+  "Quit",
+  () => (isRoomLoaded = false)
 );
